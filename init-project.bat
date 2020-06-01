882c85717b3787bf2d4043e06fa51df95b26f39a
Eugene Zaznoba <irishjimdegriz@gmail.com>
2020-06-01 23:54:00 +0300

    feat: added header and hero

--- init-project.bat
+++ init-project.bat
@@ -1,7 +1,9 @@
 call npm i -d gulp
+call npm i -d gulp-cli
 call npm i -d browser-sync
 call npm i -d sass-css
 call npm i -d gulp-autoprefixer
+call npm i -d gulp-sass
 call npm i -d gulp-clean-css
 call npm i -d gulp-minify
 call npm i -d gulp-htmlmin

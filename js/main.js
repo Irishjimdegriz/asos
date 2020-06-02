33cc1f42ba1ba10130f25c753afa53068cabde2e
Eugene <irishjimdegriz@gmail.com>
2020-06-03 01:52:42 +0300

    feat: improved styling

--- js/main.js
+++ js/main.js
@@ -32,6 +32,41 @@ $(document).ready(function () {
       noSwiping: true
     }
   });
+
+      function scrollFunction() {
+      let scrollTop = $(window).scrollTop();
+
+      if (scrollTop > 20) {
+        scrollUp.css('display', "block");
+      } else {
+        scrollUp.css('display', "none");
+      }
+    }
+    
+    $(window).scroll(scrollFunction);
+
+    // Add smooth scrolling to all links
+    $("a").on('click', function(event) {
+
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+  
+        // Store hash
+        var hash = this.hash;
+  
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){
+  
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        });
+      } // End if
+    });
 });
 // const swiperLabels = ['Выезд на замер <br>помещения', 'Составление<br> сметы', 'Разработка<br>  дизайн проекта', 'Закупка расходных<br> материалов', 'Ремонтно-отделочные<br> работы', 'Приемка-сдача <br>работ'];
 
